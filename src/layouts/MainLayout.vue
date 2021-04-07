7c48e603e7dcba6c0690c4244f06b7ab7f1ee27c
Jakeline Liima <jakeline.liima555@gmail.com>
2021-04-06 23:58:10 -0300

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,80 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Projeto em aula
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label header class="text-grey-8">
+          Menu
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <transition
+        appear
+        enter-active-class="animated fadeIn"
+        leave-active-class="animated fadeOut"
+      >
+        <!-- Wrapping only one DOM element, defined by QBtn -->
+        <q-btn color="secondary" icon="mail" label="Email" />
+      </transition>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksData = [
+  {
+    title: "Principal",
+    caption: "Tela Principal",
+    icon: "home",
+    link: "/"
+  },
+  {
+    title: "Promoções",
+    caption: "Promoções da semana",
+    icon: "money",
+    link: "/promocao"
+  }
+];
+
+export default {
+  name: "MainLayout",
+  components: { EssentialLink },
+  data() {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    };
+  }
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,82 @@
+<template>
+  <q-page class="">
+    <div class="q-pa-md">
+      <div class="q-gutter-md">
+        <q-input filled v-model="codigo" label="Codigo" />
+        <q-input outlined v-model="description" label="Descrição" />
+      </div>
+
+      <div class="q-gutter-md">
+        <q-btn
+          color="purple"
+          label="Enviar"
+          @click="adicionarCompromisso"
+          style="margin:25px auto; display: block;"
+        />
+      </div>
+    </div>
+    <q-item
+      clickable
+      v-ripple
+      v-for="(compromisso, index) in listaCompromisso"
+      :key="index"
+    >
+      <q-item-section>
+        <q-item-label
+          >{{ compromisso.codigo }} -
+          {{ compromisso.description }}</q-item-label
+        >
+        <q-btn
+          color="red"
+          label="Excluir"
+          icon="delete"
+          @click="deleteCompromisso(index)"
+          style="margin:25px auto; display: block;"
+        />
+      </q-item-section>
+    </q-item>
+  </q-page>
+</template>
+
+<script>
+import axios from 'boot/axios'
+
+export default {
+  name: "PageIndex",
+ 
+  data () {
+    return {
+      codigo: '',
+      description: '',
+      listaCompromisso: [
+        {codigo: 1, description: 'Aula'},
+        {codigo: 2,description: 'Natação'}
+      ]
+    }
+  },
+  methods: {
+    adicionarCompromisso() {
+      this.listaCompromisso.push({codigo: this.codigo, description: this.description });
+      this.codigo = ''
+      this.description = ''
+    },
+    deleteCompromisso (index) {
+      this.$q.dialog({
+        title: 'deletar',
+        message: 'Deseja excluir?',
+        cancel: true,
+        persistent: true
+      }).onOk(() => {
+        this.listaCompromisso.splice(index, 1)
+        this.$q.notify({
+        message: 'Tarefa excluida',
+        color: 'green'
+      })
+      }).onCancel(() => {
+      })
+    },
+
+     
+    }
+  }
+</script>
